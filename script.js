91af975be10ca323f6476da25736a5b82cca0f80
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-17 11:40:03 -0500

    Added collision detection between burd and burdHouse

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ let xSpeed = 60;
 let ySpeed = 120;
 
 
+
+let innerWidth = window.innerWidth;
+let innerHeight = window.innerHeight;
+
+console.log(`Inner width of window = ${innerWidth}`);
+console.log(`Inner height of window = ${innerHeight}`);
+
+
+console.log(document.getElementById("burd").getBoundingClientRect());
+console.log(document.getElementById("burdHouse").getBoundingClientRect());
+
 function myFunc() {
-  if (xSpeed*time < window.innerWidth) {
+  if (xSpeed*time < window.innerWidth) {// and no collision
     time++;
     }
   else {
-    time = 0;
+    time = 0; //there was a collision, add hit or miss to window, reset
     }
 
     document.getElementById("burd").style.left = xSpeed * time + "px";
@@ -18,7 +29,31 @@ function myFunc() {
 }
 
 
+
+
 setInterval(myFunc, 250);
+//clearInterval(myVar); //stop setInterval from running
+
+function detectCollision() {
+  let burd = document.getElementById("burd").getBoundingClientRect();
+  let house = document.getElementById("burdHouse").getBoundingClientRect();
+
+  console.log(`Burd rect = ${burd}`);
+  console.log(`House rect = ${house}`);
+
+  if (burd.left < house.left + house.width &&
+      burd.left + house.width > house.left &&
+      burd.top < house.top + house.height &&
+      burd.height + burd.top > house.top) {
+        let collided =  0;
+        collided += 1;
+        console.log(collided);
+      }
+
+
+    }
+
+
 
 
 
